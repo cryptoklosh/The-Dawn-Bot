53e7e3da0d677196b819adf2313d8648fc346b12
CrimsonAndRed <crimsonandred@mail.ru>
2024-12-24 20:47:19 +0100

    Fix metric names

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,5 +1,5 @@
 from prometheus_client import Gauge, Info, Counter
 
 dawn_info = Info("dawn", "Info about dawn-validator node")
-mined_coins_gauge = Gauge('mined_coins', 'Number of mined coins', ['account'])
-dawn_requests_total_counter = Counter('dawn_requests_total', 'Succeeded API mining requests', ['account', 'status'])
+mined_dawn_gauge = Gauge('mined_dawn', 'Number of mined dawn points', ['account'])
+dawn_requests_total_counter = Counter('dawn_requests_total', 'API mining requests', ['account', 'status'])
